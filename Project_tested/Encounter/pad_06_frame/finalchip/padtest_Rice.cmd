a40c0e5cced96a45c72e019b14262db17be1dab0
pbh2 <112043068+pbh2@users.noreply.github.com>
2023-05-01 16:12:14 -0500

    Added "Rice" test files

---
+++ Project_tested/Encounter/pad_06_frame/finalchip/padtest_Rice.cmd
@@ -0,0 +1,31 @@
+
+vector in_d_in d_in_7 d_in_6 d_in_5 d_in_4 d_in_3 d_in_2 d_in_1 d_in_0
+vector data_out out_data_15 out_data_14 out_data_13 out_data_12 out_data_11 out_data_10 out_data_9 out_data_8 out_data_7 out_data_6 out_data_5 out_data_4 out_data_3 out_data_2 out_data_1 out_data_0
+vector in_key_in key_in_7 key_in_6 key_in_5 key_in_4 key_in_3 key_in_2 key_in_1 key_in_0
+ana in_clka in_clkb in_restart in_enable_encode
+ana in_d_in data_out in_key_in
+|   cycle             1 2 3 4 5 6 7 8 9 10111213141516171819
+V   in_restart        0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+V   in_enable_encode  0 0 0 1 1 1 1 1 1 0 1 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 0 1 0 0 0 0 0 0 0
+V   d_in_7      0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 0 0 0 0 0 0 0 0 0 1 1 1 0 0 0 0 0 0
+V   d_in_6      1 1 1 1 1 1 1 1 1 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+V   d_in_5      0 0 0 0 0 1 1 1 1 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0 0
+V   d_in_4      1 1 1 1 1 0 0 0 0 1 1 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1
+V   d_in_3      0 0 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0 0 1 1 1 0 0 0 0 0 0
+V   d_in_2      0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 0 0 0 0 0 1 1 1 1 0 0 0 1 1 1 1 1 1
+V   d_in_1      1 1 1 1 1 0 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 0 1 1 1 1 1 1 1 1 1
+V   d_in_0      0 0 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+V   key_in_7    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+V   key_in_6    0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+V   key_in_5    0 0 0 0 0 1 1 1 1 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+V   key_in_4    0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+V   key_in_3    0 0 0 0 0 1 1 1 1 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+V   key_in_2    1 1 1 1 1 0 0 0 0 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+V   key_in_1    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+V   key_in_0    0 0 0 0 0 1 1 1 1 0 0 0 1 1 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+
+
+| Two phase clock with non-overlap period - same as Modelsim testbench
+clock in_clka 0 1 0 0
+clock in_clkb 0 0 0 1
+R
---
+++ Project_tested/Encounter/pad_06_frame/finalchip/test_Rice.cmd
@@ -0,0 +1,31 @@
+
+vector d_in in_d_in\[7\] in_d_in\[6\] in_d_in\[5\] in_d_in\[4\] in_d_in\[3\] in_d_in\[2\] in_d_in\[1\] in_d_in\[0\]
+vector data_out out_data\[15\] out_data\[14\] out_data\[13\] out_data\[12\] out_data\[11\] out_data\[10\] out_data\[9\] out_data\[8\] out_data\[7\] out_data\[6\] out_data\[5\] out_data\[4\] out_data\[3\] out_data\[2\] out_data\[1\] out_data\[0\]
+vector key_in in_key_in\[7\] in_key_in\[6\] in_key_in\[5\] in_key_in\[4\] in_key_in\[3\] in_key_in\[2\] in_key_in\[1\] in_key_in\[0\]
+ana in_clka in_clkb in_restart in_enable_encode
+ana d_in data_out key_in
+|   cycle             1 2 3 4 5 6 7 8 9 10111213141516171819
+V   in_restart        0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+V   in_enable_encode  0 0 0 1 1 1 1 1 1 0 1 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 0 1 0 0 0 0 0 0 0
+V   in_d_in\[7\]      0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 0 0 0 0 0 0 0 0 0 1 1 1 0 0 0 0 0 0
+V   in_d_in\[6\]      1 1 1 1 1 1 1 1 1 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+V   in_d_in\[5\]      0 0 0 0 0 1 1 1 1 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0 0
+V   in_d_in\[4\]      1 1 1 1 1 0 0 0 0 1 1 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1
+V   in_d_in\[3\]      0 0 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0 0 1 1 1 0 0 0 0 0 0
+V   in_d_in\[2\]      0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 0 0 0 0 0 1 1 1 1 0 0 0 1 1 1 1 1 1
+V   in_d_in\[1\]      1 1 1 1 1 0 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 0 1 1 1 1 1 1 1 1 1
+V   in_d_in\[0\]      0 0 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+V   in_key_in\[7\]    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+V   in_key_in\[6\]    0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+V   in_key_in\[5\]    0 0 0 0 0 1 1 1 1 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+V   in_key_in\[4\]    0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+V   in_key_in\[3\]    0 0 0 0 0 1 1 1 1 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+V   in_key_in\[2\]    1 1 1 1 1 0 0 0 0 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+V   in_key_in\[1\]    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+V   in_key_in\[0\]    0 0 0 0 0 1 1 1 1 0 0 0 1 1 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+
+
+| Two phase clock with non-overlap period - same as Modelsim testbench
+clock in_clka 0 1 0 0
+clock in_clkb 0 0 0 1
+R
